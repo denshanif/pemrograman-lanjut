a7962e697930b13417c23b9523ffe8f328fbe195
Hanif <alfatoni922@gmail.com>
2021-09-29 10:07:53 +0700

    update improve bubble sort

--- Sort/Bubble Sort/bubble-sort.c
+++ Sort/Bubble Sort/bubble-sort.c
@@ -1,46 +1,51 @@
 #include <stdio.h>
 
+// Bubble sort ascending
+void bubbleSort(int array[], int n) {
+	int i;
+	
+	for (i = 0; i < n - 1; i++)
+	{
+		int sudahTertukar = 0;
+		
+		int j;
+		
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (array[j] > array[j+1])
+	     	{
+				int tukar  = array[j];
+				array[j]   = array[j+1];
+				array[j+1] = tukar;
+				
+				sudahTertukar = 1;
+			}
+		}
+		
+		if (sudahTertukar == 0)
+			break;		
+	}	
+}
+
+// Tampilkan array hasil sorting
+void tampilArray(int array[], int n) {
+	int i;
+	
+	for (i = 0; i < n; i++) {
+		printf("%d  ", array[i]);	
+	}
+}
+
 int main()
 {
-	int array[6] = {22, 10, 15, 3, 8 , 2}, n = 6, i, j, tukar;
-
-	// Bubble sort ascending
-	for (i = 0 ; i < n - 1; i++)
-	{
-		for (j = 0 ; j < n - i - 1; j++)
-			{
-				if (array[j] > array[j+1])
-      				{
-						tukar      = array[j];
-						array[j]   = array[j+1];
-						array[j+1] = tukar;
-					}
-			}
-	}
-
-	printf("\nArray dalam urutan ascending:\n");
-
-	for (i = 0; i < n; i++)
-		printf("%d  ", array[i]);
+	int array[] = {22, 10, 15, 3, 8 , 2};
+	int n = 6;
 	
-	// Bubble sort descending	
-	for (i = 0 ; i < n - 1; i++)
-	{
-		for (j = 0 ; j < n - i - 1; j++)
-			{
-				if (array[j] < array[j+1])
-      				{
-						tukar      = array[j];
-						array[j]   = array[j+1];
-						array[j+1] = tukar;
-					}
-			}
-	}
-	
-	printf("\nArray dalam urutan descending:\n");
+	bubbleSort(array, n);
 
-	for (i = 0; i < n; i++)
-		printf("%d  ", array[i]);
+	printf("Array dalam urutan ascending:\n");
+	
+	tampilArray(array, n);
 
 	return 0;
 }
