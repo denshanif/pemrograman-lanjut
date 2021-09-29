3e1229c38d0574520d19c4dc35a9bba80d571866
Hanif <alfatoni922@gmail.com>
2021-09-29 10:21:36 +0700

    update bubble sort

--- Sort/Bubble Sort/bubble-sort.c
+++ Sort/Bubble Sort/bubble-sort.c
@@ -1,51 +1,24 @@
 #include <stdio.h>
 
-// Bubble sort ascending
-void bubbleSort(int array[], int n) {
-	int i;
-	
-	for (i = 0; i < n - 1; i++)
-	{
-		int sudahTertukar = 0;
-		
-		int j;
-		
-		for (j = 0; j < n - i - 1; j++)
-		{
-			if (array[j] > array[j+1])
-	     	{
-				int tukar  = array[j];
-				array[j]   = array[j+1];
-				array[j+1] = tukar;
-				
-				sudahTertukar = 1;
-			}
-		}
-		
-		if (sudahTertukar == 0)
-			break;		
-	}	
-}
-
-// Tampilkan array hasil sorting
-void tampilArray(int array[], int n) {
-	int i;
-	
-	for (i = 0; i < n; i++) {
-		printf("%d  ", array[i]);	
-	}
-}
-
 int main()
 {
-	int array[] = {22, 10, 15, 3, 8 , 2};
-	int n = 6;
-	
-	bubbleSort(array, n);
+	int array[] = {22, 10, 15, 3, 8 , 2}, n = 6, i, j, tukar;
+
+	// Bubble sort ascending
+	for (i = n - 1; i > 0; i--) {
+		for (j = n - 1; j >= n - i; j--) {
+			if (array[j] < array[j-1]) {
+				tukar      = array[j];
+				array[j]   = array[j-1];
+				array[j-1] = tukar;
+			}
+		}
+	}
 
 	printf("Array dalam urutan ascending:\n");
-	
-	tampilArray(array, n);
+
+	for (i = 0; i < n; i++)
+		printf("%d  ", array[i]);
 
 	return 0;
 }
